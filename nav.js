61b89bf44bd0c64b3d48d3dd3eab7f2f267bca0a
Michael Bacotti <michaelbacotti@gmail.com>
2026-05-29 14:02:22 -0400

    Book Picker: fix missing nav bars + reorder nav + fix Q3 types + rewrite quiz questions + footer

--- nav.js
+++ nav.js
@@ -26,8 +26,8 @@
    ' <div class="tab-bar-inner">',
    '  <a href="/articles.html">Articles</a>',
    '  <a href="/stories">Stories</a>',
-   '  <a href="/book-picker.html">Book Picker</a>',
    '  <a href="/reviews.html">Reviews</a>',
+   '  <a href="/book-picker.html">Book Picker</a>',
    ' </div>',
    '</div>'
   ].join('\n');
